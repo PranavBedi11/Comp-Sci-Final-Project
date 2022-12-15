c1fa92f5573e6213b667dfe69b3ebc79db049328
EyadNasser <113847293+EyadNasser@users.noreply.github.com>
2022-12-15 16:05:43 -0500

    Update index.js

--- src/pages/view-page/index.js
+++ src/pages/view-page/index.js
@@ -12,8 +12,6 @@ import "./style.css"
  * mapped over and rendered in the component's JSX.
  * The View component accepts a isAuth prop that determines whether or not the current user is
  * authenticated. If the user is authenticated and the post's author ID matches the user's ID, a
- * delete button is rendered for the post, which allows the user to delete the post when clicked.
- * The deletePost function is used to delete the post from the database.
  *
  * @returns {React.Element} A React element representing the View component.
 
@@ -22,8 +20,24 @@ import "./style.css"
 function View() {
   const [postLists, setPostList] = useState([]);
   const postsCollectionRef = collection(db, "posts");
+ /**
 
+*The useEffect hook is used to call the getPosts function when the component is mounted.
+*This ensures that the posts are retrieved and added to the component's state when the
+*component is rendered.
+*@returns {void}
+*/
   useEffect(() => {
+       
+    /**
+
+*The getPosts function is used to retrieve all posts from the database. It uses
+*the getDocs function from the firebase/firestore package to retrieve all the
+*documents in the posts collection. The retrieved posts are then processed and
+*added to the component's state using the setPostList function.
+*@async
+*@returns {Promise<void>} A promise that resolves when the operation is complete.
+*/
     const getPosts = async () => {
       const data = await getDocs(postsCollectionRef);
       setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
